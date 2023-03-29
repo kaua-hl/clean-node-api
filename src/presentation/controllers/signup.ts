bbaa049c5538c7edda2ab0c0debe834a34005bdf
Kauã <profissional.kaua@gmail.com>
2023-03-29 18:12:14 -0300

    feat: ensure SinUpController returns 400 if password confirmation fails

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -15,6 +15,7 @@ export class SignUpController implements Controller {
   handle (httpRequest: HttpRequest): HttpResponse {
     try {
       const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+
       for (const field of requiredFields) {
         if (!httpRequest.body[field]) {
           return badRequest(new MissingParamError(field))
@@ -22,9 +23,12 @@ export class SignUpController implements Controller {
       }
 
       const isValid = this.emailValidator.isValid(httpRequest.body.email)
-
       if (!isValid) {
         return badRequest(new InvalidParamError('email'))
+      }
+
+      if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+        return badRequest(new InvalidParamError('passwordConfirmation'))
       }
 
       return {
