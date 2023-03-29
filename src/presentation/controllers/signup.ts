b26eabfaf5109faddb3614cd9e0c0bae45d5c81c
Kauã <profissional.kaua@gmail.com>
2023-03-29 18:56:06 -0300

    feat: ensure SignUpController calls AddAccountWithCorrect values

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,3 +1,4 @@
+import { AddAccount } from '../../domain/usecases/add-account'
 import { InvalidParamError } from '../errors/invalid-param-error'
 import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest, serverError } from '../helpers/http-helper'
@@ -7,9 +8,11 @@ import { HttpRequest, HttpResponse } from '../protocols/http/http'
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
+  private readonly addAccount: AddAccount
 
-  constructor (emailValidator: EmailValidator) {
+  constructor (emailValidator: EmailValidator, addAccount: AddAccount) {
     this.emailValidator = emailValidator
+    this.addAccount = addAccount
   }
 
   handle (httpRequest: HttpRequest): HttpResponse {
@@ -22,7 +25,7 @@ export class SignUpController implements Controller {
         }
       }
 
-      const { email, password, passwordConfirmation } = httpRequest.body
+      const { name, email, password, passwordConfirmation } = httpRequest.body
 
       const isValid = this.emailValidator.isValid(email)
       if (!isValid) {
@@ -32,9 +35,15 @@ export class SignUpController implements Controller {
         return badRequest(new InvalidParamError('passwordConfirmation'))
       }
 
+      const account = this.addAccount.add({
+        name,
+        email,
+        password
+      })
+
       return {
         statusCode: 200,
-        body: ''
+        body: account
       }
     } catch (error) {
       return serverError()
