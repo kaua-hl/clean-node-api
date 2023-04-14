b415218df1bdbcca91134cbace2b12e3b18d18e5
Kauã <profissional.kaua@gmail.com>
2023-04-14 11:46:03 -0300

    refactor: make server error return the error stack

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -42,8 +42,8 @@ export class SignUpController implements Controller {
       })
 
       return ok(account)
-    } catch (error) {
-      return serverError()
+    } catch (error: any) {
+      return serverError(error)
     }
   }
 }
