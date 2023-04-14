5a9243353c361c11319be11092ae78d180690e5c
Kauã <profissional.kaua@gmail.com>
2023-04-14 09:56:00 -0300

    feat: ensure mongodb reconnect if client is down

--- src/infra/db/mongodb/account-repository/account-repository.ts
+++ src/infra/db/mongodb/account-repository/account-repository.ts
@@ -5,9 +5,9 @@ import { MongoHelper } from '../helpers/mongo-helper'
 
 export class AccountMongoRepository implements AddAccountRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
-    const accountCollection = MongoHelper.getCollection('accounts')
+    const accountCollection = await MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(accountData)
-    const account = { id: result.insertedId.toHexString(), ...accountData }
-    return account
+    const accountById = await accountCollection.findOne({ _id: result.insertedId })
+    return MongoHelper.map(accountById)
   }
 }
--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -2,9 +2,10 @@ import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account-repository'
 import { SignUpController } from '../../presentation/controllers/signup'
+import { Controller } from '../../presentation/protocols/controller'
 import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 
-export const makeSignUpController = (): SignUpController => {
+export const makeSignUpController = (): Controller => {
   const salt = 12
   const emailValidatorAdapter = new EmailValidatorAdapter()
   const accountMongoRepository = new AccountMongoRepository()
